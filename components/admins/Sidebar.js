68b9bd1f04383dcc748d38a031c47ec9c4e91b5f
Mbrain1 <31806957+Mbrain1@users.noreply.github.com>
2022-01-03 05:52:56 +0100

    Add files via upload

--- components/admins/Sidebar.js
+++ components/admins/Sidebar.js
@@ -1,27 +1,7 @@
 import {useState,useEffect,useContext} from 'react';
-import {useRouter} from "next/router";
-import Image from 'next/image';
-import Link from 'next/link';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Button from 'react-bootstrap/Button';
-import {UserContext} from "/components/UserContext";
-import Cookie from "js-cookie";
+
 
 const Sidebar = ({page}) => {
-
-
-  const [show, setShow] = useState(false);
-  const {setIsAdminLoggedIn} = useContext(UserContext);
-  const router = useRouter();
-
-
-
-    const logout = () => {
-      setIsAdminLoggedIn(false);
-      Cookie.remove("adminData");
-      router.push("/admin");
-    };
-
 
 
 	return (<div className="col-md-3">
