cd6a5fa7e214f8d575dfdc32cce10110e110a625
Mbrain1 <31806957+Mbrain1@users.noreply.github.com>
2022-02-10 06:46:07 +0100

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['imageserver.prod.s3.amazonaws.com','houstoncultures.com'],
+    domains: ['imageserver.prod.s3.amazonaws.com','www.houstoncultures.com','houstoncultures.com','amazonaws.com'],
   },
 }
